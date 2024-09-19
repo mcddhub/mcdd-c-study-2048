0472812e120c092629dd9f9bd3e6f65ad1bec3cd
caobaoqi1029 <2024cbq@gmail.com>
2024-09-19 09:25:30 +0800

    🧪 test: test part_01

---
+++ test_part01_init.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "part01_init.h"
+
+static void should_init_success(void **state)
+{
+    (void)state;
+    initalize_game();
+    assert_int_equal(game.current_score, 0);
+    assert_int_equal(game.game_status, 0);
+    assert_int_equal(game.available_tiles, 16);
+    for (int row = 0; row < ROW; ++row)
+    {
+        for (int col = 0; col < COL; ++col)
+        {
+            assert_int_equal(game.grid[row][col], 0);
+        }
+    }
+}
+
+int main()
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(should_init_success)};
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
